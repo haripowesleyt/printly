51b4025fda9b0d3a5a8e6424a2bb14952819fe3e
haripowesleyt <haripowesleyt@proton.me>
2025-05-24 14:40:15 +0200

    feat: add printly package

---
+++ printly/__init__.py
@@ -0,0 +1,3 @@
+"""Printly is a Python library that enhances the built-in `print()` function with direct \
+    text styling: foreground color (`fg`), background color (`bg`), and font styles (`fs`).
+"""
